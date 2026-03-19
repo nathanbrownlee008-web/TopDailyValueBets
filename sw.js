2de491786b36915c5fb045227696b6c9969cfac3
nathanbrownlee008-web <nathanbrownlee008@gmail.com>
2026-03-19 21:59:37 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Use absolute URLs so this works reliably on Vercel and ensures correct scope.
-const CACHE_NAME = "top-daily-tips-v88";
+const CACHE_NAME = "top-daily-tips-v89";
 const CORE_ASSETS = [
   "/",
   "/index.html",
